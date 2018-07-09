f79fbf5cd43525f273372e05bdf6ff1f144ee622
Jeroen Zandvliet <lissandratheicedervish@gmail.com>
2018-07-09 22:33:12 +0200

    Level slots made
    Naming headers do not align properly yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,18 @@ export class AppComponent {
     {'name': 'Wounds', 'value': 15},
     {'name': 'Movement', 'value': '5/10/15/30'}
   ];
+
+
+  spellslots = [
+    {'name': '1', 'value': 4, 'expended': 0},
+    {'name': '2', 'value': 3, 'expended': 0},
+    {'name': '3', 'value': 3, 'expended': 0},
+    {'name': '4', 'value': 3, 'expended': 0},
+    {'name': '5', 'value': 3, 'expended': 0},
+    {'name': '6', 'value': 2, 'expended': 0},
+    {'name': '7', 'value': 2, 'expended': 0},
+    {'name': '8', 'value': 1, 'expended': 0},
+    {'name': '9', 'value': 1, 'expended': 0}
+
+  ];
 }
