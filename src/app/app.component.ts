23f9100c94e5a3b75ecb5bf430b80175743db899
Jeroen Zandvliet <lissandratheicedervish@gmail.com>
2018-07-10 17:29:52 +0200

    Merge branch 'feature/creatingcombatant' into develop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,34 @@ import { Combatant } from './models/Combatant';
 })
 export class AppComponent {
 
+
+  /** Move this to radiobutton component */
+  versionChosen: string = "Dark Heresy";
+  versions: string[] = ['Dark Heresy', 'Fifth Edition'];
+
+  /** Move to topbar component */
+  title = 'D&D Initiative Tracker';
+
+  /** Temporary placeholders for testing */
+
+  heresyPlaceholder: Combatant = new Combatant("Big Mek", [{'name': 'Wounds', 'value': 15},
+    {'name': 'Movement', 'value': "5/10/15/20/25/30"}], "Does kaboom stuff.");
+  fifthPlaceholder: Combatant = new Combatant("Anathema", [{'name': 'Hit Points', 'value': 143},
+    {'name': 'Armor Class', 'value': 19}], "Does nommie stuff.", true, 15);
+
+  currentCombatant = this.heresyPlaceholder;
+
+
+
+  /** When radiobuttons are used, event used to fetch the changed value */
   changeVersion(event){
     if(event.value === "Dark Heresy"){
       this.currentCombatant = this.heresyPlaceholder;
     }
     else if(event.value === "Fifth Edition") {
       this.currentCombatant = this.fifthPlaceholder;
-    };
+    }
   };
-
-  heresyPlaceholder: Combatant = new Combatant("Big Mek", [{'name': 'Wounds', 'value': 15},
-    {'name': 'Movement', 'value': "5/10/15/20/25/30"}], "Does kaboom stuff.");
-
-  fifthPlaceholder: Combatant = new Combatant("Dancer", [{'name': 'Hit Points', 'value': 26},
-    {'name': 'Armor Class', 'value': 16}], "Does clawie stuff.");
-
-  currentCombatant = this.heresyPlaceholder;
-
-  versionChosen: string = "Dark Heresy";
-  versions: string[] = ['Dark Heresy', 'Fifth Edition'];
-
-
-  title = 'D&D Initiative Tracker';
-
 
 
   spellslots = [
@@ -44,6 +49,5 @@ export class AppComponent {
     {'name': '7', 'value': 2, 'expended': 0},
     {'name': '8', 'value': 1, 'expended': 0},
     {'name': '9', 'value': 1, 'expended': 0}
-
   ];
 }
--- src/app/models/Combatant.ts
+++ src/app/models/Combatant.ts
@@ -1,15 +1,35 @@
-export class Combatant{
+export class Combatant {
 
   name: String;
   attributes: [{}];
   description: String;
-  hasSpellSlots: boolean;
+  hasSpellSlots: Boolean;
+  casterLevel: number
+  spellslots;
 
 
-  constructor(name: String, attributes, description: String, hasSpellSlots = false){
+  constructor(name: String, attributes?, description?: String, hasSpellSlots = false, casterLevel?: number) {
     this.name = name;
     this.attributes = attributes;
     this.description = description;
     this.hasSpellSlots = hasSpellSlots;
+    this.casterLevel = casterLevel;
+    this.calculateSpellSlots(casterLevel);
+  }
+
+  calculateSpellSlots(casterLevel: number){
+    this.spellslots = [
+      {'name': 1, 'value': 4, 'expended': 0},
+      {'name': 2, 'value': 3, 'expended': 0},
+      {'name': 3, 'value': 3, 'expended': 0},
+      {'name': 4, 'value': 3, 'expended': 0},
+      {'name': 5, 'value': 3, 'expended': 0},
+      {'name': 6, 'value': 2, 'expended': 0},
+      {'name': 7, 'value': 2, 'expended': 0},
+      {'name': 8, 'value': 1, 'expended': 0},
+      {'name': 9, 'value': 1, 'expended': 0}
+    ];
   }
 }
+
+
